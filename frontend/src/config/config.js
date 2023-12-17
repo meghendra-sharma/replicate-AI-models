bba1d3b8dae6feaa02d073e4ef6fa6c21bd12cbe
meghendra-sharma <meghendrasharma3@gmail.com>
2023-12-17 18:26:48 +0530

    error fixed

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://replicate-ai-models-backend.onrender.com' :'http://localhost:5000/'
+export const BACKEND_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://replicate-ai-models-backend.onrender.com/' :'http://localhost:5000/'
